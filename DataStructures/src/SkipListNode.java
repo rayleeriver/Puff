0a4aa64eafaf65970f04ad6fe8435f0e39e89218
Raymond Lee <rayleeriver@gmail.com>
2014-04-29 09:20:06 -0400

    Initial commit

---
+++ DataStructures/src/SkipListNode.java
@@ -0,0 +1,12 @@
+/*************************  SkipListNode.java  *********************/
+
+public class SkipListNode<T> {
+    public T key;
+    public SkipListNode<T>[] next;
+    SkipListNode(T i, int n) {
+        key = i;
+        next = new SkipListNode[n];
+        for (int j = 0; j < n; j++)
+            next[j] = null;
+    }
+}
